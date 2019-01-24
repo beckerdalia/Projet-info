1c29a1647277b932acd743a8e1c9135d819d13c8
Roland Becker <roland.becker@univ-pau.fr>
2019-01-24 19:54:57 +0100

    float

--- sandkasten.py
+++ sandkasten.py
@@ -2,30 +2,33 @@ from __future__ import print_function
 from __future__ import division
 
 
-data={}
-data['justine']=[19, 12,13,14,15]
-data['hela']=[14,12,16, 14,15]
-
-def write(data):
-    with open('notes.txt','w') as file:
+#----------------------------------
+def ecriture(data, filename='notes.txt'):
+    with open(filename,'w') as file:
         for nom in data:
             file.write(nom)
             for note in data[nom]:
                 file.write(",{}".format(note))
             file.write("\n")
 
-def lecture():
+#----------------------------------
+def lecture(filename='notes.txt'):
 	data = {}
-	with open('notes.txt','r') as file:
+	with open(filename,'r') as file:
          lines = file.readlines()
          for line in lines:
              linestrip = line.strip().split(',')
              print('linestrip[0] =', linestrip[0])
              print('linestrip[1] =', linestrip[1:])
-             data[linestrip[0]] = [int(l) for l in linestrip[1:]]
+             data[linestrip[0]] = [float(l) for l in linestrip[1:]]
 	return data
 
-write(data)
+# quelques données pour tester
+data={}
+data['justine']=[19, 12,13,14,15]
+data['hela']=[14,12,16, 14,15]
+
+ecriture(data)
 data2 = lecture()
 
 print("data", data)
